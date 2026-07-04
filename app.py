dc6c912e923df5b9a8bfb999c03b81590a1d9c78
operacional4np-code <operacional4np@gmail.com>
2026-07-04 11:22:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,66 +7,56 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.units import mm
 from reportlab.platypus import Paragraph
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.enums import TA_LEFT
 
-# --- CONFIGURAÇÃO DA PÁGINA STREAMLIT ---
-st.set_page_config(page_title="Gerador de Etiquetas", page_icon="✈️", layout="centered")
+st.set_page_config(page_title="Gerador de Etiquetas", layout="centered")
 CSV_PATH = 'destinos.csv'
 
 def carregar_dataframe():
     if not os.path.exists(CSV_PATH):
         return pd.DataFrame(columns=['sigla', 'nome_recebedor', 'cnpj_recebedor', 'endereco_recebedor', 'cidade_recebedor', 'uf_recebedor', 'cep_recebedor', 'pais_recebedor'])
-    df = pd.read_csv(CSV_PATH, sep=';')
-    df.columns = df.columns.str.lower()
-    return df
+    return pd.read_csv(CSV_PATH, sep=';')
 
-def gerar_etiquetas_pdf(sigla, quantidade, dados_recebedor):
+def gerar_etiquetas_pdf(sigla, quantidade, dados):
     buffer = io.BytesIO()
     c = canvas.Canvas(buffer, pagesize=(100 * mm, 100 * mm))
     styles = getSampleStyleSheet()
     
-    # Estilo dos blocos
-    style_normal = ParagraphStyle(name='Normal', parent=styles['Normal'], fontName='Helvetica', fontSize=8, leading=10)
-    
-    # Estilo do Overpack com fonte 46 como pedido
-    style_overpack = ParagraphStyle(name='Overpack', parent=styles['Normal'], fontName='Helvetica-Bold', fontSize=46, leading=48)
-    
-    data_atual = datetime.now().strftime("%d/%m/%Y")
+    # Estilo dos blocos de texto
+    style_normal = ParagraphStyle('Normal', parent=styles['Normal'], fontName='Helvetica', fontSize=8, leading=10)
     
     for i in range(1, quantidade + 1):
-        # 1. Cabeçalho Principal (Topo)
+        # 1. Cabeçalho (Sigla + Número)
         c.setFont('Helvetica-Bold', 36)
         c.drawString(4*mm, 85*mm, sigla)
+        c.drawString(78*mm, 85*mm, f"#{i}")
         
-        numero_str = f"#{i}"
-        largura_num = c.stringWidth(numero_str, 'Helvetica-Bold', 36)
-        c.drawString(96*mm - largura_num, 85*mm, numero_str)
+        # 2. OVERPACK USED (Desenhado diretamente para garantir a fonte 46)
+        c.setFont('Helvetica-Bold', 46)
+        c.drawString(4*mm, 68*mm, "OVERPACK")
+        c.drawString(4*mm, 55*mm, "USED")
         
-        # 2. OVERPACK USED (Fonte 46 como solicitado)
-        # Usamos uma área de desenho menor para o parágrafo respeitar a largura
-        p_overpack = Paragraph("OVERPACK<br/>USED", style_overpack)
-        p_overpack.wrapOn(c, 92*mm, 50*mm)
-        p_overpack.drawOn(c, 4*mm, 50*mm)
+        # 3. Textos (Expedidor e Recebedor)
+        exp = f"<b>EXPEDIDOR:</b> NEW POST LOGISTICA ENDEREÇO: R UBALDO FAGGEANI, 355,0 - JARDIM RESIDENCIAL LAS PALMAS MUNICÍPIO: PORTO FERREIRA - SP CEP: 13660-000 CNPJ/CPF: 28.678.104/0001-79"
+        rec = f"<b>RECEBEDOR:</b> {dados['nome_recebedor']} CNPJ {dados['cnpj_recebedor']} ENDEREÇO: {dados['endereco_recebedor']}, {dados['cidade_recebedor']} - {dados['uf_recebedor']} CEP: {dados['cep_recebedor']}"
         
-        # 3. Textos (Baixados para não colidir com a fonte 46)
-        expedidor_text = f"<b>EXPEDIDOR:</b> NEW POST LOGISTICA ENDEREÇO: R UBALDO FAGGEANI, 355,0 - JARDIM RESIDENCIAL LAS PALMAS MUNICÍPIO: PORTO FERREIRA - SP CEP: 13660-000 CNPJ/CPF: 28.678.104/0001-79 IE: 555074223110 UF: SP PAÍS: BRASIL"
-        recebedor_text = f"<b>RECEBEDOR:</b> {dados_recebedor['nome_recebedor']} CNPJ {dados_recebedor['cnpj_recebedor']} ENDEREÇO: {dados_recebedor['endereco_recebedor']}, {dados_recebedor['cidade_recebedor']} - {dados_recebedor['uf_recebedor']} CEP: {dados_recebedor['cep_recebedor']}"
-        data_text = f"<b>DATA DE EXPEDIÇÃO:</b> {data_atual}"
+        p1 = Paragraph(exp, style_normal)
+        p1.wrapOn(c, 92*mm, 30*mm)
+        p1.drawOn(c, 4*mm, 30*mm)
         
-        Paragraph(expedidor_text, style_normal).wrapOn(c, 92*mm, 20*mm)
-        Paragraph(expedidor_text, style_normal).drawOn(c, 4*mm, 30*mm)
+        p2 = Paragraph(rec, style_normal)
+        p2.wrapOn(c, 92*mm, 20*mm)
+        p2.drawOn(c, 4*mm, 15*mm)
         
-        Paragraph(recebedor_text, style_normal).wrapOn(c, 92*mm, 20*mm)
-        Paragraph(recebedor_text, style_normal).drawOn(c, 4*mm, 15*mm)
-        
-        c.setFont('Helvetica-Bold', 8)
-        c.drawString(4*mm, 6*mm, data_text.replace("<b>", "").replace("</b>", ""))
+        # 4. Data
+        c.setFont('Helvetica', 8)
+        c.drawString(4*mm, 6*mm, f"DATA DE EXPEDIÇÃO: {datetime.now().strftime('%d/%m/%Y')}")
         
         c.showPage()
     c.save()
     buffer.seek(0)
     return buffer
 
-# Interface Streamlit (mantida igual)
+# --- INTERFACE ---
 st.title("✈️ Emissor de Etiquetas")
-# ... (restante do código do formulário permanece igual)
+df = carregar_dataframe()
+# ... (Manter o restante da lógica de formulário igual)
