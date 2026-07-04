ed7c8f3ab5c9414ef1cfbe7e8e092b7556374bc2
operacional4np-code <operacional4np@gmail.com>
2026-07-04 11:26:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,32 +21,35 @@ def gerar_etiquetas_pdf(sigla, quantidade, dados):
     buffer = io.BytesIO()
     c = canvas.Canvas(buffer, pagesize=(100 * mm, 100 * mm))
     styles = getSampleStyleSheet()
+    
+    # Estilo compacto para os blocos de dados
     style_normal = ParagraphStyle('Normal', parent=styles['Normal'], fontName='Helvetica', fontSize=8, leading=10)
     
     for i in range(1, quantidade + 1):
-        # 1. Cabeçalho: Sigla e Número
+        # --- 1. CABEÇALHO (Topo) ---
         c.setFont('Helvetica-Bold', 36)
-        c.drawString(4*mm, 85*mm, sigla)
-        c.drawString(78*mm, 85*mm, f"#{i}")
+        c.drawString(4*mm, 86*mm, sigla)
+        c.drawString(78*mm, 86*mm, f"#{i}")
         
-        # 2. OVERPACK USED - Fonte 40 (Desenhado fixo)
+        # --- 2. OVERPACK USED (Fonte 40) ---
         c.setFont('Helvetica-Bold', 40)
         c.drawString(4*mm, 68*mm, "OVERPACK")
         c.drawString(4*mm, 52*mm, "USED")
         
-        # 3. Textos (Expedidor e Recebedor)
-        exp = f"<b>EXPEDIDOR:</b> NEW POST LOGISTICA ENDEREÇO: R UBALDO FAGGEANI, 355,0 - JARDIM RESIDENCIAL LAS PALMAS MUNICÍPIO: PORTO FERREIRA - SP CEP: 13660-000 CNPJ/CPF: 28.678.104/0001-79"
-        rec = f"<b>RECEBEDOR:</b> {dados['nome_recebedor']} CNPJ {dados['cnpj_recebedor']} ENDEREÇO: {dados['endereco_recebedor']}, {dados['cidade_recebedor']} - {dados['uf_recebedor']} CEP: {dados['cep_recebedor']}"
+        # --- 3. BLOCOS DE TEXTO (Ajustados para não colidir com o título grande) ---
+        # Posicionamos o Expedidor logo abaixo do "USED" (52mm - 18mm de margem)
+        exp = f"<b>EXPEDIDOR:</b> NEW POST LOGISTICA | R UBALDO FAGGEANI, 355 - PORTO FERREIRA/SP | CNPJ: 28.678.104/0001-79"
+        rec = f"<b>RECEBEDOR:</b> {dados['nome_recebedor']} | {dados['endereco_recebedor']}, {dados['cidade_recebedor']}-{dados['uf_recebedor']} | CEP: {dados['cep_recebedor']}"
         
         p1 = Paragraph(exp, style_normal)
         p1.wrapOn(c, 92*mm, 30*mm)
-        p1.drawOn(c, 4*mm, 32*mm)
+        p1.drawOn(c, 4*mm, 35*mm) # Expedidor
         
         p2 = Paragraph(rec, style_normal)
         p2.wrapOn(c, 92*mm, 20*mm)
-        p2.drawOn(c, 4*mm, 15*mm)
+        p2.drawOn(c, 4*mm, 20*mm) # Recebedor
         
-        # 4. Data
+        # --- 4. DATA DE EXPEDIÇÃO (Rodapé) ---
         c.setFont('Helvetica', 8)
         c.drawString(4*mm, 6*mm, f"DATA DE EXPEDIÇÃO: {datetime.now().strftime('%d/%m/%Y')}")
         
@@ -55,7 +58,7 @@ def gerar_etiquetas_pdf(sigla, quantidade, dados):
     buffer.seek(0)
     return buffer
 
-# --- INTERFACE DO USUÁRIO ---
+# --- INTERFACE ---
 st.title("✈️ Emissor de Etiquetas")
 df = carregar_dataframe()
 
